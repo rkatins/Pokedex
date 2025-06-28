443cfe33c2906a47e6fbc7d674ba9fae7b24e97b
Rubén Katins Durán <katinsruben.98@gmail.com>
2025-06-28 20:06:30 +0200

    Establecido rutas para las navegaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+
 import { StartMenu } from './components/startMenu'
+import { Pokedex } from './components/pokedex'
+import { EquipoPokemon } from './components/equipoPokemon'
+import { TarjetaEntrenador } from './components/tarjetaEntrenador'
+import { Mochila } from './components/mochila'
+import { Ajustes } from './components/ajustes'
+import { Guardar } from './components/guardar'
 
 import "./App.css";
 import './assets/css/animacion.css'
@@ -9,7 +17,15 @@ import './assets/css/font.css'
 export function App() {
   return (
     <>
-      <StartMenu/>
+      <Routes>
+        <Route path='/' element={<StartMenu />} />
+        <Route path='/pokedex' element={<Pokedex />} />
+        <Route path='/equipoPokemon' element={<EquipoPokemon />} />
+        <Route path='/tarjetaEntrenador' element={<TarjetaEntrenador />} />
+        <Route path='/mochila' element={<Mochila />} />
+        <Route path='/ajustes' element={<Ajustes />} />
+        <Route path='/guardar' element={<Guardar />} />
+    </Routes>
     </>
   );
 };
